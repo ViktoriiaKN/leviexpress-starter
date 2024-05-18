2333393c49251c39bf934288106ffe3c713f59c6
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-18 14:16:48 +0200

    Delete console.log(cities)

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const CityOptions = ({cities}) => {
-  console.log(cities);
   return (
     <>
       <option value="">Vyberte</option>
